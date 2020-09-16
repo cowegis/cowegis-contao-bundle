85d28551eec70294d4fa59ad3b155c026dc4f006
David Molineus <david.molineus@netzmacht.de>
2020-09-16 09:11:26 +0200

    Add missing translations

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Translations are managed using Transifex. To create a new translation
- * or to help to maintain an existing one, please register at transifex.com.
- *
- * @link    http://help.transifex.com/intro/translating.html
- * @link    https://www.transifex.com/projects/p/contao-leaflet-maps/language/de/
- *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2015-02-11T08:57:53+01:00
- */
 
 $GLOBALS['TL_LANG']['MOD']['cowegis_layer']['0']   = 'Layer';
 $GLOBALS['TL_LANG']['MOD']['cowegis_layer']['1']   = 'Kartenlayer verwalten';
@@ -18,18 +7,18 @@ $GLOBALS['TL_LANG']['MOD']['cowegis_map']['1']     = 'Cowegis Karten verwalten';
 $GLOBALS['TL_LANG']['MOD']['cowegis_presets']['0'] = 'Vorlagen';
 $GLOBALS['TL_LANG']['MOD']['cowegis_presets']['1'] = 'Coewgis Vorlagen verwalten';
 
-$GLOBALS['TL_LANG']['CTE']['leaflet']['0']       = 'Leaflet-Karte';
-$GLOBALS['TL_LANG']['CTE']['leaflet']['1']       = 'Leaflet-Karte als Inhaltselement integrieren.';
-$GLOBALS['TL_LANG']['FMD']['leaflet']['0']       = 'Leaflet-Karte';
-$GLOBALS['TL_LANG']['FMD']['leaflet']['1']       = 'Leaflet-Karte als Frontend-Modul integrieren.';
-$GLOBALS['TL_LANG']['MOD']['leaflet']['0']       = 'Leaflet';
-$GLOBALS['TL_LANG']['MOD']['leaflet']['1']       = 'Leaflet-Erweiterung für Contao';
-$GLOBALS['TL_LANG']['MOD']['leaflet_about']['0'] = 'Über';
-$GLOBALS['TL_LANG']['MOD']['leaflet_about']['1'] = 'Über die Leaflet-Integration für Contao';
+$GLOBALS['TL_LANG']['CTE']['cowegis_map']['0']   = 'Cowegis-Karte';
+$GLOBALS['TL_LANG']['CTE']['cowegis_map']['1']   = 'Cowegis-Karte als Inhaltselement integrieren.';
+$GLOBALS['TL_LANG']['FMD']['cowegis_map']['0']   = 'Cowegis-Karte';
+$GLOBALS['TL_LANG']['FMD']['cowegis_map']['1']   = 'Cowegis-Karte als Frontend-Modul integrieren.';
+$GLOBALS['TL_LANG']['MOD']['cowegis']['0']       = 'Cowegis';
+$GLOBALS['TL_LANG']['MOD']['cowegis']['1']       = 'Cowegis-Erweiterung für Contao';
+$GLOBALS['TL_LANG']['MOD']['cowegis_about']['0'] = 'Über';
+$GLOBALS['TL_LANG']['MOD']['cowegis_about']['1'] = 'Über die Cowegis-Integration für Contao';
 
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_control'] = 'Kontrollelemente';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_icon']    = 'Icons';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_layer']   = 'Kartenlayer';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_map']     = 'Karten';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_marker']  = 'Marker';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_control'] = 'Kontrollelemente';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_icon']    = 'Icons';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_layer']   = 'Kartenlayer';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_map']     = 'Karten';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_marker']  = 'Marker';
 
--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -17,19 +17,17 @@ $GLOBALS['TL_LANG']['MOD']['cowegis_layer'][1]   = 'Manage map layers';
 $GLOBALS['TL_LANG']['MOD']['cowegis_presets'][0] = 'Presets';
 $GLOBALS['TL_LANG']['MOD']['cowegis_presets'][1] = 'Manage style presets';
 
-$GLOBALS['TL_LANG']['MOD']['leaflet'][0]       = 'Leaflet';
-$GLOBALS['TL_LANG']['MOD']['leaflet'][1]       = 'Leaflet extension for Contao';
-$GLOBALS['TL_LANG']['MOD']['leaflet_about'][0] = 'About';
-$GLOBALS['TL_LANG']['MOD']['leaflet_about'][1] = 'About the Leaflet integration for Contao';
+$GLOBALS['TL_LANG']['MOD']['cowegis'][0]       = 'Cowegis';
+$GLOBALS['TL_LANG']['MOD']['cowegis'][1]       = 'Cowegis - Content Web GIS for Contao';
 
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_map']     = 'Maps';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_layer']   = 'Layers';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_icon']    = 'Icons';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_marker']  = 'Markers';
-$GLOBALS['TL_LANG']['MOD']['tl_leaflet_control'] = 'Controls';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_map']     = 'Maps';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_layer']   = 'Layers';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_icon']    = 'Icons';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_marker']  = 'Markers';
+$GLOBALS['TL_LANG']['MOD']['tl_cowegis_control'] = 'Controls';
 
-$GLOBALS['TL_LANG']['FMD']['leaflet'][0] = 'Leaflet Map';
-$GLOBALS['TL_LANG']['FMD']['leaflet'][1] = 'Integrate leaflet map as frontend module.';
+$GLOBALS['TL_LANG']['FMD']['cowegis_map'][0] = 'Cowegis Map';
+$GLOBALS['TL_LANG']['FMD']['cowegis_map'][1] = 'Integrate cowegis map as frontend module.';
 
-$GLOBALS['TL_LANG']['CTE']['leaflet'][0] = 'Leaflet Map';
-$GLOBALS['TL_LANG']['CTE']['leaflet'][1] = 'Integrate leaflet map as content element.';
+$GLOBALS['TL_LANG']['CTE']['cowegis_map'][0] = 'Cowegis Map';
+$GLOBALS['TL_LANG']['CTE']['cowegis_map'][1] = 'Integrate cowegis map as content element.';
