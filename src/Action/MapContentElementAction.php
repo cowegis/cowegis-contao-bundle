57eb8d95539a1a2bbdc3406286de3fe969e0e126
David Molineus <david.molineus@netzmacht.de>
2024-08-05 15:32:45 +0200

    Implement __invoke method
    The fragment registry seems to be buggy with a specific method name.

--- src/Action/MapContentElementAction.php
+++ src/Action/MapContentElementAction.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace Cowegis\Bundle\Contao\Action;
 
+use Contao\ContentModel;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
 use Contao\Model;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+#[AsContentElement('cowegis_map', 'includes', 'ce_cowegis_map')]
 final class MapContentElementAction extends MapFragmentAction
 {
+    public function __invoke(Request $request, ContentModel $model, string $section, ?array $classes = null,): Response
+    {
+        return parent::renderAsContentElement($request, $model, $section, $classes);
+    }
+
     protected function getIdentifier(Model $model, string|null $identifier): string
     {
         if ($identifier === null) {
