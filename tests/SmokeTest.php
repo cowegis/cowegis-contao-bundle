23dd89f1612cc3fd2e379cb1f3bc41a790eedb79
David Molineus <david.molineus@netzmacht.de>
2026-09-01 10:14:32 +0200

    Add PHPUnit via companion/phpcq alongside phpspec

---
+++ tests/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cowegis\Bundle\Contao\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testPhpunitRuns(): void
+    {
+        self::assertTrue(true);
+    }
+}
