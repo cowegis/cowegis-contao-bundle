60e50f826447539043d1f47d136835a8cfde54eb
David Molineus <david.molineus@netzmacht.de>
2026-09-02 13:23:30 +0200

    Fix docs backend view (#26)

--- src/Action/Backend/DocsAction.php
+++ src/Action/Backend/DocsAction.php
@@ -4,30 +4,28 @@ declare(strict_types=1);
 
 namespace Cowegis\Bundle\Contao\Action\Backend;
 
+use Contao\CoreBundle\Controller\Backend\AbstractBackendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
-use Twig\Environment;
 
-final class DocsAction
+final class DocsAction extends AbstractBackendController
 {
-    public function __construct(private readonly Environment $twig, private readonly RouterInterface $router)
+    public function __construct(private readonly RouterInterface $router)
     {
     }
 
     public function __invoke(Request $request): Response
     {
-        return new Response(
-            $this->twig->render(
-                '@CowegisContao/backend/docs.html.twig',
-                [
-                    'schemaUri' => $this->router->generate('cowegis_api_docs_schema'),
-                    'backUrl'   => $this->router->generate(
-                        'contao_backend',
-                        ['do' => 'cowegis_map', 'ref' => $request->attributes->get('_contao_referer_id')],
-                    ),
-                ],
-            ),
+        return $this->render(
+            '@CowegisContao/backend/docs.html.twig',
+            [
+                'schemaUri' => $this->router->generate('cowegis_api_docs_schema'),
+                'backUrl'   => $this->router->generate(
+                    'contao_backend',
+                    ['do' => 'cowegis_map', 'ref' => $request->attributes->get('_contao_referer_id')],
+                ),
+            ],
         );
     }
 }
